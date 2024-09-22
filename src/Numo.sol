9cf4c7abb48bc383abf755cf3938ce7a70cd7c09
Robert Leifke <r.leifke@gmail.com>
2024-09-21 23:52:16 -0400

    removed deployment scripts

--- src/Numo.sol
+++ src/Numo.sol
@@ -1,92 +1,92 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
-import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+// import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+// import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+// import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+// import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+// import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+// import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+// import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
 
-contract Numo is BaseHook {
-    using PoolIdLibrary for PoolKey;
+// contract Numo is BaseHook {
+//     using PoolIdLibrary for PoolKey;
 
-    // NOTE: ---------------------------------------------------------
-    // state variables should typically be unique to a pool
-    // a single hook contract should be able to service multiple pools
-    // ---------------------------------------------------------------
+//     // NOTE: ---------------------------------------------------------
+//     // state variables should typically be unique to a pool
+//     // a single hook contract should be able to service multiple pools
+//     // ---------------------------------------------------------------
 
-    uint8[] public decimals;
+//     uint8[] public decimals;
 
-    constructor(
-                IPoolManager _poolManager, 
-                IERC20[] memory _pooledTokens, 
-                uint8[] memory _decimals
-    )
-        BaseHook(_poolManager)
-    {
-        // Check _pooledTokens and precisions parameter
-        require(_pooledTokens.length == 2, "_pooledTokens.length == 2");  // Enforce that there are exactly 2 tokens
-        require(_pooledTokens.length == _decimals.length, "_pooledTokens decimals mismatch");  // Ensure token/decimal match
+//     constructor(
+//                 IPoolManager _poolManager, 
+//                 IERC20[] memory _pooledTokens, 
+//                 uint8[] memory _decimals
+//     )
+//         BaseHook(_poolManager)
+//     {
+//         // Check _pooledTokens and precisions parameter
+//         require(_pooledTokens.length == 2, "_pooledTokens.length == 2");  // Enforce that there are exactly 2 tokens
+//         require(_pooledTokens.length == _decimals.length, "_pooledTokens decimals mismatch");  // Ensure token/decimal match
         
-        decimals = _decimals;  // Store the decimals array
-    }
-    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
-        return Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: true,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: true,
-            afterRemoveLiquidity: false,
-            beforeSwap: true,
-            afterSwap: true,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
-    }
+//         decimals = _decimals;  // Store the decimals array
+//     }
+//     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+//         return Hooks.Permissions({
+//             beforeInitialize: false,
+//             afterInitialize: false,
+//             beforeAddLiquidity: true,
+//             afterAddLiquidity: false,
+//             beforeRemoveLiquidity: true,
+//             afterRemoveLiquidity: false,
+//             beforeSwap: true,
+//             afterSwap: true,
+//             beforeDonate: false,
+//             afterDonate: false,
+//             beforeSwapReturnDelta: false,
+//             afterSwapReturnDelta: false,
+//             afterAddLiquidityReturnDelta: false,
+//             afterRemoveLiquidityReturnDelta: false
+//         });
+//     }
 
-    // -----------------------------------------------
-    // NOTE: see IHooks.sol for function documentation
-    // -----------------------------------------------
+//     // -----------------------------------------------
+//     // NOTE: see IHooks.sol for function documentation
+//     // -----------------------------------------------
 
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
-        external
-        override
-        returns (bytes4, BeforeSwapDelta, uint24)
-    {
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
-    }
+//     function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+//         external
+//         override
+//         returns (bytes4, BeforeSwapDelta, uint24)
+//     {
+//         return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+//     }
 
-    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
-        external
-        override
-        returns (bytes4, int128)
-    {
-        return (BaseHook.afterSwap.selector, 0);
-    }
+//     function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+//         external
+//         override
+//         returns (bytes4, int128)
+//     {
+//         return (BaseHook.afterSwap.selector, 0);
+//     }
 
-    function beforeAddLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        return BaseHook.beforeAddLiquidity.selector;
-    }
+//     function beforeAddLiquidity(
+//         address,
+//         PoolKey calldata key,
+//         IPoolManager.ModifyLiquidityParams calldata,
+//         bytes calldata
+//     ) external override returns (bytes4) {
+//         return BaseHook.beforeAddLiquidity.selector;
+//     }
 
-    function beforeRemoveLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        return BaseHook.beforeRemoveLiquidity.selector;
-    }
-}
+//     function beforeRemoveLiquidity(
+//         address,
+//         PoolKey calldata key,
+//         IPoolManager.ModifyLiquidityParams calldata,
+//         bytes calldata
+//     ) external override returns (bytes4) {
+//         return BaseHook.beforeRemoveLiquidity.selector;
+//     }
+// }
--- test/Numo.t.sol
+++ test/Numo.t.sol
@@ -1,124 +1,124 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "forge-std/Test.sol";
-import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
-import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import {TickMath} from "v4-core/src/libraries/TickMath.sol";
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
-import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
-import {Numo} from "../src/Numo.sol";
-import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+// import "forge-std/Test.sol";
+// import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+// import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+// import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+// import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+// import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+// import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+// import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+// import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+// import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
+// import {Numo} from "../src/Numo.sol";
+// import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
 
-import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
-import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
-import {EasyPosm} from "./utils/EasyPosm.sol";
-import {Fixtures} from "./utils/Fixtures.sol";
+// import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+// import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+// import {EasyPosm} from "./utils/EasyPosm.sol";
+// import {Fixtures} from "./utils/Fixtures.sol";
 
-contract NumoTest is Test, Fixtures {
-    using EasyPosm for IPositionManager;
-    using PoolIdLibrary for PoolKey;
-    using CurrencyLibrary for Currency;
-    using StateLibrary for IPoolManager;
+// contract NumoTest is Test, Fixtures {
+//     using EasyPosm for IPositionManager;
+//     using PoolIdLibrary for PoolKey;
+//     using CurrencyLibrary for Currency;
+//     using StateLibrary for IPoolManager;
 
-    Numo amm;
-    PoolId poolId;
+//     Numo amm;
+//     PoolId poolId;
 
-    uint256 tokenId;
-    int24 tickLower;
-    int24 tickUpper;
+//     uint256 tokenId;
+//     int24 tickLower;
+//     int24 tickUpper;
 
-    function setUp() public {
-        // creates the pool manager, utility routers, and test tokens
-        deployFreshManagerAndRouters();
-        deployMintAndApprove2Currencies();
+//     function setUp() public {
+//         // creates the pool manager, utility routers, and test tokens
+//         deployFreshManagerAndRouters();
+//         deployMintAndApprove2Currencies();
 
-        deployAndApprovePosm(manager);
+//         deployAndApprovePosm(manager);
 
-        // Deploy the hook to an address with the correct flags
-        address flags = address(
-            uint160(
-                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                    | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
-            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
-        );
-        bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
-        deployCodeTo("Counter.sol:Counter", constructorArgs, flags);
-        amm = Numo(flags);
+//         // Deploy the hook to an address with the correct flags
+//         address flags = address(
+//             uint160(
+//                 Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+//                     | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+//             ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+//         );
+//         bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
+//         deployCodeTo("Counter.sol:Counter", constructorArgs, flags);
+//         amm = Numo(flags);
 
-        // Create the pool
-        key = PoolKey(currency0, currency1, 3000, 60, IHooks(amm));
-        poolId = key.toId();
-        manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+//         // Create the pool
+//         key = PoolKey(currency0, currency1, 3000, 60, IHooks(amm));
+//         poolId = key.toId();
+//         manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
 
-        // Provide full-range liquidity to the pool
-        tickLower = TickMath.minUsableTick(key.tickSpacing);
-        tickUpper = TickMath.maxUsableTick(key.tickSpacing);
+//         // Provide full-range liquidity to the pool
+//         tickLower = TickMath.minUsableTick(key.tickSpacing);
+//         tickUpper = TickMath.maxUsableTick(key.tickSpacing);
 
-        uint128 liquidityAmount = 100e18;
+//         uint128 liquidityAmount = 100e18;
 
-        // (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
-        //     SQRT_PRICE_1_1,
-        //     TickMath.getSqrtPriceAtTick(tickLower),
-        //     TickMath.getSqrtPriceAtTick(tickUpper),
-        //     liquidityAmount
-        // );
+//         // (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
+//         //     SQRT_PRICE_1_1,
+//         //     TickMath.getSqrtPriceAtTick(tickLower),
+//         //     TickMath.getSqrtPriceAtTick(tickUpper),
+//         //     liquidityAmount
+//         // );
 
-        // (tokenId,) = posm.mint(
-        //     key,
-        //     tickLower,
-        //     tickUpper,
-        //     liquidityAmount,
-        //     amount0Expected + 1,
-        //     amount1Expected + 1,
-        //     address(this),
-        //     block.timestamp,
-        //     ZERO_BYTES
-        // );
-    }
+//         // (tokenId,) = posm.mint(
+//         //     key,
+//         //     tickLower,
+//         //     tickUpper,
+//         //     liquidityAmount,
+//         //     amount0Expected + 1,
+//         //     amount1Expected + 1,
+//         //     address(this),
+//         //     block.timestamp,
+//         //     ZERO_BYTES
+//         // );
+//     }
 
-    function testCounterHooks() public {
-        // positions were created in setup()
-        assertEq(amm.beforeAddLiquidityCount(poolId), 1);
-        assertEq(amm.beforeRemoveLiquidityCount(poolId), 0);
+//     function testCounterHooks() public {
+//         // positions were created in setup()
+//         assertEq(amm.beforeAddLiquidityCount(poolId), 1);
+//         assertEq(amm.beforeRemoveLiquidityCount(poolId), 0);
 
-        assertEq(amm.beforeSwapCount(poolId), 0);
-        assertEq(amm.afterSwapCount(poolId), 0);
+//         assertEq(amm.beforeSwapCount(poolId), 0);
+//         assertEq(amm.afterSwapCount(poolId), 0);
 
-        // Perform a test swap //
-        bool zeroForOne = true;
-        int256 amountSpecified = -1e18; // negative number indicates exact input swap!
-        BalanceDelta swapDelta = swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
-        // ------------------- //
+//         // Perform a test swap //
+//         bool zeroForOne = true;
+//         int256 amountSpecified = -1e18; // negative number indicates exact input swap!
+//         BalanceDelta swapDelta = swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+//         // ------------------- //
 
-        assertEq(int256(swapDelta.amount0()), amountSpecified);
+//         assertEq(int256(swapDelta.amount0()), amountSpecified);
 
-        assertEq(amm.beforeSwapCount(poolId), 1);
-        assertEq(amm.afterSwapCount(poolId), 1);
-    }
+//         assertEq(amm.beforeSwapCount(poolId), 1);
+//         assertEq(amm.afterSwapCount(poolId), 1);
+//     }
 
-    function testLiquidityHooks() public {
-        // positions were created in setup()
-        assertEq(amm.beforeAddLiquidityCount(poolId), 1);
-        assertEq(amm.beforeRemoveLiquidityCount(poolId), 0);
+//     function testLiquidityHooks() public {
+//         // positions were created in setup()
+//         assertEq(amm.beforeAddLiquidityCount(poolId), 1);
+//         assertEq(amm.beforeRemoveLiquidityCount(poolId), 0);
 
-        // // remove liquidity
-        // uint256 liquidityToRemove = 1e18;
-        // posm.decreaseLiquidity(
-        //     tokenId,
-        //     liquidityToRemove,
-        //     MAX_SLIPPAGE_REMOVE_LIQUIDITY,
-        //     MAX_SLIPPAGE_REMOVE_LIQUIDITY,
-        //     address(this),
-        //     block.timestamp,
-        //     ZERO_BYTES
-        // );
+//         // // remove liquidity
+//         // uint256 liquidityToRemove = 1e18;
+//         // posm.decreaseLiquidity(
+//         //     tokenId,
+//         //     liquidityToRemove,
+//         //     MAX_SLIPPAGE_REMOVE_LIQUIDITY,
+//         //     MAX_SLIPPAGE_REMOVE_LIQUIDITY,
+//         //     address(this),
+//         //     block.timestamp,
+//         //     ZERO_BYTES
+//         // );
 
-        assertEq(amm.beforeAddLiquidityCount(poolId), 1);
-        assertEq(amm.beforeRemoveLiquidityCount(poolId), 1);
-    }
-}
+//         assertEq(amm.beforeAddLiquidityCount(poolId), 1);
+//         assertEq(amm.beforeRemoveLiquidityCount(poolId), 1);
+//     }
+// }
