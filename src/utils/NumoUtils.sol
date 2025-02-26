98caf7893ed06001fe173d411306dd5cb2705174
Robert Leifke <r.leifke@gmail.com>
2025-02-26 10:05:16 -0500

    delete:

--- src/utils/NumoUtils.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-library MarketUtils {
-    uint256 public constant POOL_PRECISION_DECIMALS = 18;
-    uint256 public constant MAX_SWAP_FEE = 1e17; // 10%
-    uint256 public constant MAX_ADMIN_FEE = 1e17; // 10%
-}
