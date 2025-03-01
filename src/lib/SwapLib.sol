7b9b05253bc1b426cf841420e719c7ad37512099
Robert Leifke <r.leifke@gmail.com>
2025-03-01 17:25:45 -0500

    chore: remove pendle tokens

--- src/lib/SwapLib.sol
+++ src/lib/SwapLib.sol
@@ -365,67 +365,67 @@ library SwapLib {
         return result;
     }
 
-    function calcMaxPtIn(
-        uint256 reserveX_,
-            uint256 reserveY_,
-            uint256 totalLiquidity_,
-            uint256 strike_
-        ) public pure returns (uint256) {
-            uint256 low = 0;
-            uint256 high = reserveY_ - 1;
+    // function calcMaxPtIn(
+    //     uint256 reserveX_,
+    //         uint256 reserveY_,
+    //         uint256 totalLiquidity_,
+    //         uint256 strike_
+    //     ) public pure returns (uint256) {
+    //         uint256 low = 0;
+    //         uint256 high = reserveY_ - 1;
 
-            while (low != high) {
-                uint256 mid = (low + high + 1) / 2;
-                if (calcSlope(reserveX_, reserveY_, totalLiquidity_, strike_, int256(mid)) < 0) {
-                    high = mid - 1;
-                } else {
-                    low = mid;
-                }
-            }
+    //         while (low != high) {
+    //             uint256 mid = (low + high + 1) / 2;
+    //             if (calcSlope(reserveX_, reserveY_, totalLiquidity_, strike_, int256(mid)) < 0) {
+    //                 high = mid - 1;
+    //             } else {
+    //                 low = mid;
+    //             }
+    //         }
 
-            return low;
-        }
+    //         return low;
+    //     }
 
-    function calcSlope(
-        uint256 reserveX_,
-        uint256 reserveY_,
-        uint256 totalLiquidity_,
-        uint256 strike_,
-        int256 ptToMarket
-    ) public pure returns (int256) {
-        uint256 newReserveY = reserveY_ + uint256(ptToMarket);
-        uint256 b_i = newReserveY * 1e36 / (strike_ * totalLiquidity_);
+    // function calcSlope(
+    //     uint256 reserveX_,
+    //     uint256 reserveY_,
+    //     uint256 totalLiquidity_,
+    //     uint256 strike_,
+    //     int256 ptToMarket
+    // ) public pure returns (int256) {
+    //     uint256 newReserveY = reserveY_ + uint256(ptToMarket);
+    //     uint256 b_i = newReserveY * 1e36 / (strike_ * totalLiquidity_);
 
-        if (b_i > 1e18) {
-            return -1;
-        }
+    //     if (b_i > 1e18) {
+    //         return -1;
+    //     }
         
-        int256 b = Gaussian.ppf(toInt(b_i));
-        int256 pdf_b = Gaussian.pdf(b);
+    //     int256 b = Gaussian.ppf(toInt(b_i));
+    //     int256 pdf_b = Gaussian.pdf(b);
         
-        int256 slope = (int256(strike_ * totalLiquidity_) * pdf_b / 1e36);
+    //     int256 slope = (int256(strike_ * totalLiquidity_) * pdf_b / 1e36);
         
-        int256 dxdy = computedXdY(reserveX_, newReserveY);
+    //     int256 dxdy = computedXdY(reserveX_, newReserveY);
         
-        return slope + dxdy;
-    }
+    //     return slope + dxdy;
+    // }
 
-    function calcMaxPtOut(
-        uint256 reserveX_,
-        uint256 reserveY_,
-        uint256 totalLiquidity_,
-        uint256 strike_,
-        uint256 sigma_,
-        uint256 tau_
-    ) public pure returns (uint256) {
-        int256 currentTF = computeTradingFunction(reserveX_, reserveY_, totalLiquidity_, strike_, sigma_, tau_);
+    // function calcMaxPtOut(
+    //     uint256 reserveX_,
+    //     uint256 reserveY_,
+    //     uint256 totalLiquidity_,
+    //     uint256 strike_,
+    //     uint256 sigma_,
+    //     uint256 tau_
+    // ) public pure returns (uint256) {
+    //     int256 currentTF = computeTradingFunction(reserveX_, reserveY_, totalLiquidity_, strike_, sigma_, tau_);
         
-        uint256 maxProportion = uint256(int256(1e18) - currentTF) * 1e18 / (2 * 1e18);
+    //     uint256 maxProportion = uint256(int256(1e18) - currentTF) * 1e18 / (2 * 1e18);
         
-        uint256 maxPtOut = reserveY_ * maxProportion / 1e18;
+    //     uint256 maxPtOut = reserveY_ * maxProportion / 1e18;
         
-        return (maxPtOut * 999) / 1000;
-    }
+    //     return (maxPtOut * 999) / 1000;
+    // }
 
 
     function computedXdY(
