dc098032c8aaf68309e65ab4cc2aec4a0aeecb15
Robert Leifke <r.leifke@gmail.com>
2025-04-22 00:01:37 -0400

    feat: add test

--- test/Numo.t.sol
+++ test/Numo.t.sol
@@ -1,118 +1,92 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {TestSetup} from "./helpers/TestSetup.sol";
-import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
-import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
-import {BaseCustomAccounting} from "@uniswap-hooks/base/BaseCustomAccounting.sol";
-import {BeforeSwapDelta} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
+import "forge-std/Test.sol";
+import {Numo} from "src/Numo.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 
-contract NumoTest is TestSetup {
-    PoolKey internal key;
-    bytes internal emptyHookData;
+contract NumoTest is Test {
+    Numo numo;
+    MockPoolManager poolManager;
 
-    function setUp() public override {
-        super.setUp();
+    address user = address(0xBEEF);
 
-        key = PoolKey({
-            currency0: Currency.wrap(address(0)),
-            currency1: Currency.wrap(address(1)),
+    function setUp() public {
+        poolManager = new MockPoolManager();
+        numo = new Numo(IPoolManager(address(poolManager)), 1e18, 0.1e18); // mean=1, width=0.1
+    }
+
+    function testAddLiquidity() public {
+        vm.prank(user);
+        (uint256 amount0, uint256 amount1, uint256 shares) = numo._getAmountIn(
+            Numo.AddLiquidityParams(1000e18, 1000e18, 0, 0)
+        );
+
+        assertGt(shares, 0);
+        assertEq(numo.totalLiquidity(), shares);
+        assertEq(numo.reserve0(), amount0);
+        assertEq(numo.reserve1(), amount1);
+    }
+
+    function testRemoveLiquidity() public {
+        vm.startPrank(user);
+        numo._getAmountIn(Numo.AddLiquidityParams(1000e18, 1000e18, 0, 0));
+        vm.stopPrank();
+
+        uint256 prevLiquidity = numo.totalLiquidity();
+
+        vm.prank(user);
+        (uint256 amount0, uint256 amount1, uint256 shares) = numo._getAmountOut(
+            Numo.RemoveLiquidityParams(prevLiquidity)
+        );
+
+        assertEq(numo.reserve0(), 0);
+        assertEq(numo.reserve1(), 0);
+        assertEq(numo.totalLiquidity(), 0);
+    }
+
+    function testSimpleSwapZeroForOne() public {
+        vm.startPrank(user);
+        numo._getAmountIn(Numo.AddLiquidityParams(1000e18, 1000e18, 0, 0));
+        vm.stopPrank();
+
+        vm.prank(user);
+        (,,uint24 fee) = numo._beforeSwap(user, PoolKey({
+            currency0: IPoolManager.Currency.wrap(address(0)),
+            currency1: IPoolManager.Currency.wrap(address(1)),
             fee: 0,
-            tickSpacing: 1,
-            hooks: hook
-        });
+            tickSpacing: 1
+        }), IPoolManager.SwapParams({
+            zeroForOne: true,
+            amountSpecified: int256(10e18),
+            sqrtPriceLimitX96: 0
+        }), "");
 
-        emptyHookData = "";
+        assertEq(fee, 0);
+        assertGt(numo.reserve0(), 1000e18);
+        assertLt(numo.reserve1(), 1000e18);
     }
 
-    function test_SpotPriceAtMaturity() public {
-        IPoolManager.SwapParams memory params =
-            IPoolManager.SwapParams({zeroForOne: true, amountSpecified: 1e18, sqrtPriceLimitX96: 0});
+    function testSimpleSwapOneForZero() public {
+        vm.startPrank(user);
+        numo._getAmountIn(Numo.AddLiquidityParams(1000e18, 1000e18, 0, 0));
+        vm.stopPrank();
 
-        uint256 preBefore = hook.getSpotPrice();
-        hook.beforeSwap(address(0), key, params, emptyHookData);
-        uint256 preAfter = hook.getSpotPrice();
-        assertGt(preBefore, 0, "Price should be non-zero before maturity");
+        vm.prank(user);
+        (,,uint24 fee) = numo._beforeSwap(user, PoolKey({
+            currency0: IPoolManager.Currency.wrap(address(0)),
+            currency1: IPoolManager.Currency.wrap(address(1)),
+            fee: 0,
+            tickSpacing: 1
+        }), IPoolManager.SwapParams({
+            zeroForOne: false,
+            amountSpecified: int256(10e18),
+            sqrtPriceLimitX96: 0
+        }), "");
 
-        vm.warp(maturity);
-        vm.roll(maturity);
-
-        uint256 price = hook.getSpotPrice();
-        assertEq(price, strike, "Price should equal strike at maturity");
-    }
-
-    function test_AddLiquidity() public {
-        IPoolManager.ModifyLiquidityParams memory params = IPoolManager.ModifyLiquidityParams({
-            tickLower: -1,
-            tickUpper: 1,
-            liquidityDelta: int256(1e18),
-            salt: bytes32(0)
-        });
-
-        uint256 totalLiquidityBefore = hook.totalLiquidity();
-        hook.beforeAddLiquidity(address(this), key, params, "");
-        uint256 totalLiquidityAfter = hook.totalLiquidity();
-
-        assertGt(totalLiquidityAfter, totalLiquidityBefore, "Liquidity should increase");
-    }
-
-    function test_RemoveLiquidity() public {
-        IPoolManager.ModifyLiquidityParams memory addParams = IPoolManager.ModifyLiquidityParams({
-            tickLower: -1,
-            tickUpper: 1,
-            liquidityDelta: int256(1e18),
-            salt: bytes32(0)
-        });
-        hook.beforeAddLiquidity(address(this), key, addParams, "");
-
-        IPoolManager.ModifyLiquidityParams memory removeParams = IPoolManager.ModifyLiquidityParams({
-            tickLower: -1,
-            tickUpper: 1,
-            liquidityDelta: -int256(1e18),
-            salt: bytes32(0)
-        });
-
-        uint256 totalLiquidityBefore = hook.totalLiquidity();
-        hook.beforeRemoveLiquidity(address(this), key, removeParams, "");
-        uint256 totalLiquidityAfter = hook.totalLiquidity();
-
-        assertLt(totalLiquidityAfter, totalLiquidityBefore, "Liquidity should decrease");
-    }
-
-    function test_PrepareInit() public {
-        uint256 priceX = 1000e18; // $1000
-        uint256 amountX = 1e18; // 1 token
-
-        (uint256 totalLiquidity_, uint256 amountY) = hook.prepareInit(priceX, amountX, strike, sigma);
-
-        assertGt(totalLiquidity_, 0, "Total liquidity should be non-zero");
-        assertGt(amountY, 0, "Amount Y should be non-zero");
-    }
-
-    function test_RevertAfterMaturity() public {
-        vm.warp(maturity + 1);
-
-        IPoolManager.ModifyLiquidityParams memory params = IPoolManager.ModifyLiquidityParams({
-            tickLower: -1,
-            tickUpper: 1,
-            liquidityDelta: int256(1e18),
-            salt: bytes32(0)
-        });
-
-        vm.expectRevert("Expired, Cannot Add Liquidity");
-        hook.beforeAddLiquidity(address(this), key, params, "");
-    }
-
-    function test_GetUnspecifiedAmount() public {
-        IPoolManager.SwapParams memory params =
-            IPoolManager.SwapParams({zeroForOne: true, amountSpecified: 1e18, sqrtPriceLimitX96: 0});
-
-        (bytes4 selector, BeforeSwapDelta delta,) = hook.beforeSwap(address(0), key, params, emptyHookData);
-        assertEq(selector, hook.beforeSwap.selector, "Should return correct selector");
-
-        vm.warp(maturity);
-        (selector, delta,) = hook.beforeSwap(address(0), key, params, emptyHookData);
-        assertEq(selector, hook.beforeSwap.selector, "Should return correct selector at maturity");
+        assertEq(fee, 0);
+        assertGt(numo.reserve1(), 1000e18);
+        assertLt(numo.reserve0(), 1000e18);
     }
 }
